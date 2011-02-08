e954a0d41af696ab4bf7a28ea77d2321c359ccca
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2011-02-07 16:51:50 -0800

    fixed a couple of really obvious bugs. More stability with normal numbering. moving is still wierd.

--- numtab.js
+++ numtab.js
@@ -1,16 +1,18 @@
-chrome.extension.onRequest.addListener(updateTitle);
-var old_id;
-function updateTitle(id){
-	var title = document.title;
-	console.log(id, old_id, title);
-	if(id !== old_id){
-		var len = 0;
-		if(old_id){
-			len = old_id.toString().length
+(function(){
+	chrome.extension.onRequest.addListener(updateTitle);
+	var old_id;
+	function updateTitle(id){
+		// maybe work catching and triggering after a 100 millis just to ensure proper setting
+		var title = document.title;
+		if(id !== old_id){
+			var len = 0;
+			if(old_id){
+				len = old_id.toString().length
+			}
+			title = id + " " + title.substr(len);
+			//console.log(title);
+			document.title = title;
+			old_id = id;
 		}
-		title = id + " " + title.substr(len);
-		console.log(title);
-		document.title = title;
-		old_id = id;
 	}
-}
+})()
