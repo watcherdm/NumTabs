84601fec3010f72d9dba5b76fc06ace06132b0ed
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2011-02-13 08:03:24 -0800

    updated manifest file, resolved some bugs with  moving tabs around.

--- numtab.js
+++ numtab.js
@@ -3,16 +3,18 @@
 	var old_id;
 	function updateTitle(id){
 		// maybe work catching and triggering after a 100 millis just to ensure proper setting
-		var title = document.title;
-		if(id !== old_id){
-			var len = 0;
-			if(old_id){
-				len = old_id.toString().length
+		setTimeout(function(){
+			var title = document.title;
+			if(id !== old_id){
+				var len = 0;
+				if(old_id){
+					len = old_id.toString().length
+				}
+				title = id + " " + title.substr(len);
+				//console.log(title);
+				document.title = title;
+				old_id = id;
 			}
-			title = id + " " + title.substr(len);
-			//console.log(title);
-			document.title = title;
-			old_id = id;
-		}
+		}, 100);
 	}
 })()
